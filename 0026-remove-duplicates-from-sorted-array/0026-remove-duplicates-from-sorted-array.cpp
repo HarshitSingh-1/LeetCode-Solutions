defe4639b20178c78f3d73b454e05c2f1d9c0eb6
HarshitSingh-1 <harshit.singh.swe@gmail.com>
2026-06-27 19:28:01 +0530

    Time: 4 ms (8.25%), Space: 22.7 MB (52.45%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -7,6 +7,7 @@ public:
             for(int j=i+1;j<nums.size();j++){
                 if(nums[i]==nums[j]){
                     x=1;
+                    break;
                 }
             }
             if(x==0){
